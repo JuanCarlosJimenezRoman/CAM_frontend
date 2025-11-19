05670ee7dc82fa16495025271b0cf56d9ef0adf8
Juan Carlos Jimenez Roman <juanjimenezcarlosroman@gmail.com>
2025-11-18 18:44:51 -0600

    feat: update API URL in environment files to use consistent backend endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,3 +2,4 @@ export const environment = {
   production: true,
   apiUrl: 'https://cam-app-backend-mqet.onrender.com/api',
 };
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://cam-app-backend-pegw.onrender.com/api',
+  apiUrl: 'https://cam-app-backend-mqet.onrender.com/api',
 };
 //actuzalice la url del api
+
 
